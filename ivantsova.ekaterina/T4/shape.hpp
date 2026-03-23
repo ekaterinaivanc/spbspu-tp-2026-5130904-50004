879f957b399d33dd9e1e4b0a7fa1fb2789366a75
ekaterinaivanc <kataivancova7@gmail.com>
2026-03-23 16:03:32 +0300

    add rectangle.hpp

--- ivantsova.ekaterina/T4/shape.hpp
+++ ivantsova.ekaterina/T4/shape.hpp
@@ -9,6 +9,7 @@ namespace ivantsova {
   public:
     virtual ~Shape() = default;
     virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t& newPos) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double coefficient) = 0;
