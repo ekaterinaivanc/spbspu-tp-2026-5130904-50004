8525adbd4409880aec5caa14024cb5a52e95b713
ekaterinaivanc <kataivancova7@gmail.com>
2026-03-23 15:43:23 +0300

    add shape.hpp

---
+++ ivantsova.ekaterina/T4/shape.hpp
@@ -0,0 +1,18 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "point.hpp"
+#include "rectangle_type.hpp"
+
+namespace ivantsova {
+  class Shape {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual void move(const point_t& newPos) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double coefficient) = 0;
+  };
+}
+
+#endif
