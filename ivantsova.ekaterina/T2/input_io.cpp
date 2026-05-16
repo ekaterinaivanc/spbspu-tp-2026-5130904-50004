744495e342a85d1e876791405b3fda3cf47377e5
ekaterinaivanc <kataivancova7@gmail.com>
2026-05-16 11:45:10 +0300

    Add implementation for DoubleSci operators

--- ivantsova.ekaterina/T2/input_io.cpp
+++ ivantsova.ekaterina/T2/input_io.cpp
@@ -1,4 +1,7 @@
 #include "input_io.hpp"
+#include <iomanip>
+#include <cmath>
+#include <limits>
 
 ivantsova::IOGuard::IOGuard(std::basic_ios< char >& s) :
   s_(s),
@@ -13,4 +16,44 @@ ivantsova::IOGuard::~IOGuard() {
   s_.width(width_);
   s_.flags(flags_);
   s_.fill(fill_);
+}
+
+std::istream& ivantsova::operator>>(std::istream& is, ivantsova::DoubleSci& ds) {
+  std::istream::sentry s(is);
+  if (!s) {
+    return is;
+  }
+  ivantsova::IOGuard guard(is);
+  double mantissa_int = 0, mantissa_frac = 0;
+  char dot = 0, e_char = 0, sign = 0;
+  int exponent = 0;
+
+  is >> mantissa_int >> dot >> mantissa_frac >> e_char >> sign >> exponent;
+  if (is && dot == '.' && (e_char == 'e' || e_char == 'E')) {
+    int frac_digits = 1;
+    double temp = mantissa_frac;
+    while (temp >= 10) {
+      temp /= 10;
+      frac_digits++;
+    }
+    double mantissa = mantissa_int + mantissa_frac / std::pow(10.0, frac_digits);
+    ds.value = mantissa * std::pow(10.0, (sign == '-' ? -exponent : exponent));
+  } else {
+    is.setstate(std::ios_base::failbit);
+  }
+  return is;
+}
+
+std::ostream& ivantsova::operator<<(std::ostream& os, const ivantsova::DoubleSci& ds) {
+  ivantsova::IOGuard guard(os);
+  os << std::scientific << std::nouppercase << std::setprecision(1) << ds.value;
+  return os;
+}
+
+bool ivantsova::operator<(const ivantsova::DoubleSci& lhs, const ivantsova::DoubleSci& rhs) {
+  return lhs.value < rhs.value;
+}
+
+bool ivantsova::operator==(const ivantsova::DoubleSci& lhs, const ivantsova::DoubleSci& rhs) {
+  return std::abs(lhs.value - rhs.value) < std::numeric_limits< double >::epsilon();
 }
